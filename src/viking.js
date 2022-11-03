f97bc49e58c80387bddfb61d5dec7c45c7346330
David Acuna <david.acunag@gmail.com>
2022-11-03 20:44:40 +0000

    soldier

--- src/viking.js
+++ src/viking.js
@@ -1,5 +1,19 @@
 // Soldier
-class Soldier {}
+
+class Soldier {
+    constructor(health, strengh){
+    this.health = health,
+    this.strengh = strengh,
+}
+
+attack(){
+    return this.strengh;
+}
+
+receiveDamage(damage){
+    this.health -= damage;
+}
+
 
 // Viking
 class Viking {}
